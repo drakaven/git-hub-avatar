33947aa50227a756fd4461ba88d0bc06e64ede73
matthew jenkins <drakaven@hotmail.com>
2017-01-16 18:39:42 +0000

    step 4 added request url with username and access token

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,14 @@
 var request = require('request');
+var GITHUB_USER = "drakaven";
+var GITHUB_TOKEN = "e40639e04429fc58aa5b981d58f243e0dcde314d";
+
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  cb("error");
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
