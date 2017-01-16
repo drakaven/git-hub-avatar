2cc50e2bd048d0afa454d4c82ac7d83259e18ba5
matthew jenkins <drakaven@hotmail.com>
2017-01-16 19:09:31 +0000

    step 5 added request and user agent support

--- download_avatars.js
+++ download_avatars.js
@@ -6,9 +6,21 @@ var GITHUB_TOKEN = "e40639e04429fc58aa5b981d58f243e0dcde314d";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+const getRepoContributors = function(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'drakaven'
+    }
+  };
+
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
+  })
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
